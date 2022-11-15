bbd5143839e69f79f25e1a2c5e73962eaa4845ec
blue00419 <33975530+blue00419@users.noreply.github.com>
2022-11-16 01:54:32 +0900

    make tab and 4 fragment

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.test2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    TimelineFragment timelineFragment;
+    TicketboxFragment ticketboxFragment;
+    SettingFragment settingFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        timelineFragment = new TimelineFragment();
+        ticketboxFragment = new TicketboxFragment();
+        settingFragment = new SettingFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+                        return true;
+                    case R.id.timeline:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, timelineFragment).commit();
+                        return true;
+                    case R.id.ticketbox:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, ticketboxFragment).commit();
+                        return true;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, settingFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
